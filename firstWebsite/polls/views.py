06a8e92608d21e2fd2970d9533c93f8567788615
fnsne <watashiwafnsne@gmail.com>
2018-03-21 16:30:31 +0800

    using shortcut render to load template and pass context to the template

--- firstWebsite/polls/views.py
+++ firstWebsite/polls/views.py
@@ -3,15 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from polls.models import Question
-from django.template import loader
 
 def index(request):
 	lastest_question_list = Question.objects.order_by('pub_date')[:5]
-	template = loader.get_template('polls/index.html')
 	context = {
 		'lastest_question_list': lastest_question_list
 	}
-	return HttpResponse(template.render(context, request))
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
