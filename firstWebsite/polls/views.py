e2907a7ff58b3a0284d08d7f8550d704206bbf2c
fnsne <watashiwafnsne@gmail.com>
2018-03-21 15:58:59 +0800

    let index of polls really do things

--- firstWebsite/polls/views.py
+++ firstWebsite/polls/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from polls.models import Question
 
 def index(request):
-	return HttpResponse("this is index")
+	lastest_question_list = Question.objects.order_by('pub_date')[:5]
+	question_names = []
+	for q in lastest_question_list:
+		question_names.append(q.question_text)
+	output = '<br>'.join(question_names)
+	return HttpResponse(output)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
